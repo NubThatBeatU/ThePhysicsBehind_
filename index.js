bd388b6098972de5942013d045b59dafffa99fe5
NubThatBeatU <37029942+NubThatBeatU@users.noreply.github.com>
2018-07-19 17:26:08 -0700

    dropdown 3

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
